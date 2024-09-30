1e4e11dc888d3f44fec050249ccc1136849761e0
mdmansooralam <mdmansooralam@outlook.in>
2024-09-30 10:27:20 +0530

    initial files

---
+++ src/utility/validation.py
@@ -0,0 +1,39 @@
+import re
+
+def name_validate(name):
+
+    pattern  = r"^[A-Za-z].*$"
+
+    if(re.match(pattern, name)):
+        return name
+    else:
+        return False
+
+def price_validate(num):
+    pattern = r'^[1-9]\d*$'
+    if(re.match(pattern, num)):
+        return float(num)
+    else:
+        return False
+
+def quantity_validate(num):
+    pattern = r'^[1-9]\d*$'
+    if(re.match(pattern, num)):
+        return int(num)
+    else:
+        return False
+    
+def id_validate(id):
+    pattern = r"^[A-Za-z0-9]{4}$"
+    if(re.match(pattern, id)):
+        return id
+    else:
+        return False
+
+def check_blank(string):
+    pattern = r'^(?!-)\S'
+    if(re.match(pattern, string)):
+        return string
+    else:
+        return False
+
